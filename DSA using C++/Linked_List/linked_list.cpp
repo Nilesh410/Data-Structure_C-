a81f2288f2c3e15218b6ca90e77be3d2075b44e2
Nilesh410 <shirudenileshs@gmail.com>
2025-05-24 23:57:03 +0530

    example of linked list using C++

---
+++ DSA using C++/Linked_List/linked_list.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public: int data;
+            node *next;
+            node()
+            {
+                next=NULL;
+            }
+};
+class linked_list
+{
+    node * head;
+    public: linked_list()
+            {
+                head=NULL;
+            }
+            void insert_@_begin(int value)
+            {
+                   
+            }
+}
+int main()
+{
+    return 0;
+}
