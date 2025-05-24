d24eeb7ea131998205302217bf56f294cce71d33
Nilesh410 <shirudenileshs@gmail.com>
2025-05-25 00:13:41 +0530

    add function insert at first and display for SLL

--- DSA using C++/Linked_List/linked_list.cpp
+++ DSA using C++/Linked_List/linked_list.cpp
@@ -9,19 +9,39 @@ class node
                 next=NULL;
             }
 };
-class linked_list
+class Linked_List
 {
-    node * head;
-    public: linked_list()
+    node  *head;
+    public: Linked_List()
             {
                 head=NULL;
             }
-            void insert_@_begin(int value)
+            void insert_at_begin(int value)
             {
-                   
+                node *temp=new node();
+                temp->data=value;
+                temp->next=head;
+                head=temp;
             }
-}
+            void display()
+            {
+                if(head==NULL)
+                    cout<<"Linked List is empty";
+                node *temp=head;
+                while(temp!=NULL)
+                {
+                    cout<<temp->data<<"->";
+                    temp=temp->next;
+                }
+            }
+};
 int main()
 {
+    cout<<"Creat a linked:";
+    Linked_List l1;
+    l1.insert_at_begin(10);
+    l1.insert_at_begin(20);
+    l1.insert_at_begin(30);
+    l1.display();
     return 0;
 }
