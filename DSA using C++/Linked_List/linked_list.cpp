426b54b287cfbc77410bf817275b89c963bb48d0
Nilesh410 <shirudenileshs@gmail.com>
2025-05-25 10:40:57 +0530

    add menu drive program

--- DSA using C++/Linked_List/linked_list.cpp
+++ DSA using C++/Linked_List/linked_list.cpp
@@ -81,7 +81,7 @@ class Linked_List
                 }
                  temp->next=NULL;
             }
-            void delete_at_middle(int num)
+            void delete_at_anywhere(int num)
             {
                 if(head==NULL)
                 {
@@ -114,20 +114,55 @@ int main()
 {
     cout<<"Creat a linked:";
     Linked_List l1;
-    l1.insert_at_begin(10);
-    l1.insert_at_begin(20);
-    l1.insert_at_begin(30);
-    l1.display();
-    l1.insert_at_last(40);
-    l1.insert_at_last(50);
-    l1.display();
-    l1.insert_at_middle(23,40);
-    l1.display();
-    l1.delete_first();
-     l1.display();
-     l1.delete_last();
-     l1.display();
-     l1.delete_at_middle(20);
-     l1.display();
+    int choice,num,position,ch;
+    do{
+        cout<<"Select the operation:"<<endl;
+        cout<<"1.Insert at Begin"<<endl;
+        cout<<"2.Insert at last"<<endl;
+        cout<<"3.Insert at middle"<<endl;
+        cout<<"4.Delete at begin"<<endl;
+        cout<<"5.Delete at last"<<endl;
+        cout<<"6.Delete any where"<<endl;
+        cout<<"7.Display"<<endl;
+        cin>>choice;
+        switch(choice)
+        {
+            case 1: cout<<"enter the element to insert at begin:";
+                    cin>>num;
+                    l1.insert_at_begin(num);
+                    l1.display();
+                    break;
+            case 2:cout<<"enter the element to insert at last:";
+                    cin>>num;
+                    l1.insert_at_last(num);
+                    l1.display();
+                    break;
+            case 3: cout<<"select the positon:";
+                    cin>>position;
+                    cout<<"enter the element to insert at position:";
+                    cin>>num;
+                    l1.insert_at_middle(num,position);
+                    l1.display();
+                    break;
+            case 4:l1.delete_first();
+                    l1.display();
+                    break;
+            case 5:l1.delete_last();
+                    l1.display();
+                    break;
+            case 6: cout<<"enter the ele to delete:";
+                    cin>>num;
+                    l1.delete_at_anywhere(num);
+                    l1.display();
+                    break;
+            case 7:l1.display();
+                    break;
+            default:cout<<"Please select the valid case:";   
+                    break;
+        }
+        
+        cout<<"If you want to continue then press 1 otherwise 0";
+        cin>>ch;
+    }while(ch!=0);
     return 0;
 }
