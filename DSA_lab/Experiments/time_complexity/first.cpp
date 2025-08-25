48f8331572c11585d6461a3fff1ab66541054134
Nilesh410 <shirudenileshs@gmail.com>
2025-08-25 23:59:33 +0530

    add time complexity statement

--- DSA_lab/Experiments/time_complexity/first.cpp
+++ DSA_lab/Experiments/time_complexity/first.cpp
@@ -3,22 +3,24 @@ using namespace std;
 int main()
 {
     int x=10;
-    for(int i=n;i>0;i/=2)
-    {
-        for(int j=0;j<i;j++)
-        {
-            x=x+1;
-        }
-    }
-
     for(int i=1;i<=n;i++)
     {
         for(int j=i;j<=n;j++)
         {
-            for(int k=j;k<=n;k++)
-            {
-                
-            }
+            x=x+1;
+        }
+    }
+int i=1;
+while(i<n)
+{
+    i*=3;
+    cout<<i;
+}
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j*j<=n;j++)
+        {
+           cout<<"Hello programmer";   
         }
     }
     return 0;
